7e671b5736721c6c4753b092f38ffec34bd82927
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2025-07-13 01:16:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ import carRouter from './routes/car.routes.js';
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({extended: true, limit: '10mb'}));
