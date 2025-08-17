15df0b75d58747af28002f991dcba07170d3b271
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2025-08-17 07:43:04 +0530

    Update car.models.js

--- models/car.models.js
+++ models/car.models.js
@@ -1,98 +1,211 @@
-import mongoose from 'mongoose'
-import validator from 'validator'
+// import mongoose from 'mongoose'
+// import validator from 'validator'
 
-const carSchema = new mongoose.Schema({
+// const carSchema = new mongoose.Schema({
+//     make: String,
+//     model: String,
+//     variant: String,
+//     year: Number,
+//     price: Number,
+//     displayImg: {
+//         public_id: String,
+//         url: String
+//     },
+//     image2Avatar: {
+//         public_id: String,
+//         url: String
+//     },
+//     sellerId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     },
+//     buyerId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     },
+//     image3Avatar: {
+//         public_id: String,
+//         url: String
+//     },
+//     image4Avatar: {
+//         public_id: String,
+//         url: String
+//     },
+//     fuel: String,
+//     transmission: String,
+//     kms: Number,
+//     location: String,   
+//     owner: {
+//         type: String,
+//         // enum: ["First", "Second", "Third"]
+//     },
+//     color: String,
+//     bodyType: String,
+//     engine: String,
+//     mileage: String,
+//     seatingCapacity: {
+//         type: String,
+//         // enum: ["2", "5", "7", "9"]
+//     },
+//     registrationNumber: String,
+//     registrationState: String,
+//     insuranceValidity: String,
+//     specifications: {
+//         bootSpace: String,
+//         groundClearance: String,
+//         length: String,
+//         width: String,
+//         height: String,
+//     },
+//     verified: Boolean,
+//     inspectionReport: {
+//         exterior: String,
+//         interior: String,
+//         engine: String,
+//         electrical: String,
+//         ac: String,
+//         tyres: String,
+//     },
+//     testDrive: [
+//         {
+//             user: {
+//                 type: mongoose.Schema.Types.ObjectId,
+//                 ref: 'User',
+//             },
+//             approved: {
+//                 type: Boolean,
+//                 default: false,
+//             },
+//             day:{
+//                 type: String
+//             },
+//             time:{
+//                 type:String
+//             },
+//             name:{
+//                 type:String
+//             },
+//             phone:{
+//                 type:String
+//             }
+//         }
+//     ]
+// }, {
+//     timestamps: true
+// });
+
+// const car = mongoose.model('Car', carSchema);
+
+// export default car;
+
+
+
+
+import mongoose from "mongoose";
+import validator from "validator";
+
+const carSchema = new mongoose.Schema(
+  {
     make: String,
     model: String,
     variant: String,
     year: Number,
     price: Number,
     displayImg: {
-        public_id: String,
-        url: String
+      public_id: String,
+      url: String,
     },
     image2Avatar: {
-        public_id: String,
-        url: String
+      public_id: String,
+      url: String,
     },
     sellerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    sellerDetail: {
+      type: String,
+      validate: [validator.isEmail, "Enter validate email address"],
     },
     buyerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     image3Avatar: {
-        public_id: String,
-        url: String
+      public_id: String,
+      url: String,
     },
     image4Avatar: {
-        public_id: String,
-        url: String
+      public_id: String,
+      url: String,
     },
     fuel: String,
     transmission: String,
     kms: Number,
-    location: String,   
+    location: String,
     owner: {
-        type: String,
-        // enum: ["First", "Second", "Third"]
+      type: String,
+      // enum: ["First", "Second", "Third"]
     },
     color: String,
     bodyType: String,
     engine: String,
     mileage: String,
     seatingCapacity: {
-        type: String,
-        // enum: ["2", "5", "7", "9"]
+      type: String,
+      // enum: ["2", "5", "7", "9"]
     },
     registrationNumber: String,
     registrationState: String,
     insuranceValidity: String,
     specifications: {
-        bootSpace: String,
-        groundClearance: String,
-        length: String,
-        width: String,
-        height: String,
+      sellerName: String,
+      sellerEmailId: String,
+      sellerContactNo: String,
+      sellerWhatsappNo: String,
+      sellerLocation: String,
     },
     verified: Boolean,
     inspectionReport: {
-        exterior: String,
-        interior: String,
-        engine: String,
-        electrical: String,
-        ac: String,
-        tyres: String,
+      exterior: String,
+      interior: String,
+      engine: String,
+      electrical: String,
+      ac: String,
+      tyres: String,
     },
     testDrive: [
-        {
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-            },
-            approved: {
-                type: Boolean,
-                default: false,
-            },
-            day:{
-                type: String
-            },
-            time:{
-                type:String
-            },
-            name:{
-                type:String
-            },
-            phone:{
-                type:String
-            }
-        }
-    ]
-}, {
-    timestamps: true
-});
+      {
+        user: {
+          approved: {
+            type: Boolean,
+            default: false,
+          },
+        },
+        approved: {
+            type: Boolean,
+            default: false,
+          },
 
-const car = mongoose.model('Car', carSchema);
+        day: {
+          type: String,
+        },
+        time: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        phone: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const car = mongoose.model("Car", carSchema);
 export default car;
